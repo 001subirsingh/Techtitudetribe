a37251b68ad09861b10938d8e58da1ed42fe5a3f
SUBIR SINGH <67018565+001subirsingh@users.noreply.github.com>
2024-06-29 19:55:12 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,112 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>TechtitudeTribe</title>
+    <link rel="icon" type="image/png" sizes="16x16" href="static/logo.png">
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <style>
+*{
+    font-family: sherif;
+}
+
+footer-bottom{
+    background-color: rgba(211,211,211,1);
+}
+.lit{
+    position: relative;
+    display:block;
+}
+
+.lit-it a{
+    color: #000;
+    list-style-type: circle;
+}
+
+.social{
+    background-color: white;
+    border-radius:50%;
+    margin: 3px 4px;
+    font-size: 12px;
+}
+ .wid, ul{
+    width: fit-content;
+}
+.social a{
+    color: white;
+    border-radius: 50%;
+
+
+}
+.social a img{
+    width: 40px;
+    height: 40px;
+}
+</style>
+<footer>
+<hr>
+<div class="container">
+<div class="row">
+<div class="col-lg-3 col-sm-6">
+<div class="container txt">
+<h4> About Us</h4>
+<p style="text-align:justify;">Our motive is also to promote make-in-India moment. We are currently working and helping implement 6 great innovative ideas into the real world so that young entrepreneurs from around the nation may get help.</p>
+</div>
+</div>
+<div class="col-lg-3 col-sm-6">
+<h4>Important links</h4>
+<div class="lit-it pl-3">
+  <a class="lit" href="/">Home</a>
+  <a class="lit" href="/company/">About Us</a>
+  <a class="lit" href="/services/">Service</a>
+  <a class="lit" href="/products/">Products</a>
+  <a class="lit" href="/projects/">Projects</a>
+  <a class="lit" href="/blogs/">Blogs</a>
+  <a class="lit" href="/career/">Career</a>
+  <a class="lit" href="/contact/">Contact Us</a>
+</div>
+</div>
+<div class="col-lg-3 col-sm-6">
+<h4>Services</h4>
+<div class="lit-it pl-3">
+  <a class="lit" href="/services/#app">Android App Development</a>
+  <a class="lit" href="/services/#web">Web App Development</a>
+ 
+  <a class="lit" href="/services/#marketing">Digital Marketing</a>
+  <a class="lit" href="/services/#graphic">Graphic Designing</a>
+  
+</div>
+
+</div>
+<div class="col-lg-3 col-sm-6">
+<h4>Contact Us</h4>
+<div class="">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3485.113389974443!2d78.27095241435855!3d29.13184576805869!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390b972107a88479%3A0xe103047cc77b583!2sTechtiutde%20Tribe%20LLP!5e0!3m2!1sen!2sin!4v1626947675317!5m2!1sen!2sin" width="200" height="200" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+</div>
+</div>
+</div>
+</div>
+<hr>
+<div class="container footer-bottom p-auto">
+<ul class="mx-auto">
+<li class="social"><a target="_blank" href="https://www.facebook.com/techtitudetribellp" ><img src="static/facebook.png" class="img-fluid"></span></a></li>
+<li class="social"><a target="_blank" href="https://www.instagram.com/techtitude_tribe/?utm_medium=copy_link" ><img src="static/instagram.png" class="img-fluid"></a></li>
+<li class="social"><a target="_blank" href="https://www.linkedin.com/company/techtitudetribe/" ><img src="static/linkedin.png" class="img-fluid"></a></li>
+<li class="social"><a target="_blank" href="https://twitter.com/TechtitudeT?s=20" ><img src="static/twitter.png" class="img-fluid"></a></li>
+</ul>
+<p class="wid mx-auto p-auto text-center">&copy copyright  TechtitudeTribe 2021, &reg All rights reserved.</p>
+</div>
+</div>
+</footer>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
